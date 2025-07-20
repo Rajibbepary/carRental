386f35750fbea7b222a346c010b7c4f9df4c5305
Rajibbepary <rajib635356@gmail.com>
2025-07-20 10:04:28 +0600

    create bookingRoute and import changeBookingStatus, checkAvailabilityOfCar, createBooking, getOwnerBookings, getUserBookings

--- server/controllers/ownerController.js
+++ server/controllers/ownerController.js
@@ -1,4 +1,6 @@
+import { create } from "domain";
 import imagekit from "../configs/imagekit.js";
+import Booking from "../models/Bookings.js";
 import Car from "../models/Car.js";
 import User from "../models/User.js";
 import fs from "fs";
@@ -122,7 +124,22 @@ export const getDashboardData = async (req, res) =>{
             return res.json({ success: false, message: "Unauthorized"});
         }
         const cars = await Car.find({owner: _id})
-          return res.json({ success: true, cars });
+        const bookings = await Booking.find({owner: _id}).populate('car').sort({ createdAt: -1});
+
+        const pendingBookings = await Booking.find({owner: _id, status:"pending"})
+         const completedBookings = await Booking.find({owner: _id, status:"confirmed"})
+//Calculate monthlyRevenue from booking where status is confirmed
+        const monthlyRevenue = bookings.slice().filter(booking => booking.status === 'confirmed').reduce((acc, booking)=> acc + booking.price, 0)
+
+      const dashboardData = {
+        totalCars: cars.length,
+        totalBookings: bookings.length,
+        pendingBookings: pendingBookings.length,
+        completedBookings: completedBookings.length,
+        recentBookings: bookings.slice(0, 3),
+        monthlyRevenue
+      }  
+      res.json({ success: true, dashboardData})
     } catch (error){
         console.log(error.message)
         res.json({success: false, message: error.message})
